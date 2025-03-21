104a4cd38a290a7962b3102530a1f42cc00980dc
Abubeker-Sultan <abubeker.sultan06@gmail.com>
2025-03-21 12:28:46 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const menuIcon = document.querySelector('.menu-icon');
-const menu = document.querySelector('.main-lists-ul');
-
-menuIcon.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    menuIcon.classList.toggle('hidden'); 
-});
